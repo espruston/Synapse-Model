c3235866e50e89b3a479f1fda37e736c5fb45fbc
Evan Spruston <espruston@gmail.com>
2020-07-13 16:55:54 -0600

    Segregate methods for each dataset

--- Two_Pool_and_Maturation/Maturation_PF.py
+++ Two_Pool_and_Maturation/Maturation_PF.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from TwoPoolAndMat import two_pool
+import os
+from TwoPoolAndMat import maturation
 from matplotlib.widgets import Slider, RadioButtons
 from matplotlib import pyplot as plt
 from sklearn import metrics
@@ -31,79 +32,72 @@ if __name__ == "__main__":
 
     EPSClabels = {'1hz': EPSC_1hz, '2hz': EPSC_2hz, '10hz': EPSC_10hz, '20hz': EPSC_20hz, '50hz': EPSC_50hz, '100hz': EPSC_100hz}
 
-    r = 1
-
-    m = .89
-    size_fast = m
-    size_slow = 1-size_fast
-
-    p_fast = 0.56
-    p_slow = 1-.198
-
-    T_fast = .2
-    T_slow = 7
-
-    F = 1
-    K_F = 1.5
-    delta_F = 1
-    T_F = 0.07
-    BG_F = 1e-5
-    sat_F = K_F
-    facil = BG_F
-
+    n_sites = 1
     n_pulses = 20
+    r = 1
+    delta_t = 1000/r
+
+    #release probabilities
+    p_immature = 0.01
+    p_mature = 0.05
+    p_facilitated = .1
+
+    #time constants
+    T_refill = 160
+    T_maturation = 2200
+    T_facilitation = 100
+
     n_consider = 20
 
-    output = two_pool(r, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F)
+    output = maturation(r, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation)
 
     fig, ax = plt.subplots()
     plt.subplots_adjust(left=0.25, bottom=0.25)
 
     l, = plt.plot(range(n_pulses), output.EPSC, label = "EPSC")
-    m, = plt.plot(range(n_pulses), output.fastEPSC, label = "Fast pool")
-    n, = plt.plot(range(n_pulses), output.slowEPSC, label = "Slow pool")
+    m, = plt.plot(range(n_pulses), output.EPSC_immature, label = "Immature")
+    n, = plt.plot(range(n_pulses), output.EPSC_mature, label = "Mature")
+    o, = plt.plot(range(n_pulses), output.EPSC_facilitated, label = "Facilitated")
     q, = plt.plot(range(n_pulses), EPSC_1hz, 'b.')
     plt.xlim(0,n_pulses)
     plt.ylim(0,3)
 
-    axcolor = 'lightgoldenrodyellow'
-    axpf = plt.axes([0.25, 0.025, 0.65, 0.01], facecolor=axcolor)
-    axps= plt.axes([0.25, 0.05, 0.65, 0.01], facecolor=axcolor)
-    axT_f= plt.axes([0.25, 0.075, 0.65, 0.01], facecolor=axcolor)
-    axT_s= plt.axes([0.25, 0.1, 0.65, 0.01], facecolor=axcolor)
-    axini_f= plt.axes([0.25, 0.125, 0.65, 0.01], facecolor=axcolor)
-    axdelta_F= plt.axes([0.25, 0.15, 0.65, 0.01], facecolor=axcolor)
 
-    spf = Slider(axpf, 'p_fast', 0, 1, valinit=p_fast, valstep=.01)
-    sps = Slider(axps, 'p_slow', .1, 1, valinit=p_slow, valstep=.01)
-    sT_f = Slider(axT_f, 'T_fast', 0.01, 10, valinit=T_fast, valstep=0.01)
-    sT_s = Slider(axT_s, 'T_slow', 0.01, 10, valinit=T_slow, valstep=0.01)
-    sini_f = Slider(axini_f, 'size_fast', 0, 1, valinit=size_fast, valstep=.01)
-    sdelta_F = Slider(axdelta_F, 'delta_F', 0.01, 100, valinit=delta_F, valstep=0.01)
+    axcolor = 'lightgoldenrodyellow'
+    axpim = plt.axes([0.25, 0.025, 0.65, 0.01], facecolor=axcolor)
+    axpma= plt.axes([0.25, 0.05, 0.65, 0.01], facecolor=axcolor)
+    axpfa= plt.axes([0.25, 0.075, 0.65, 0.01], facecolor=axcolor)
+    axT_re= plt.axes([0.25, 0.1, 0.65, 0.01], facecolor=axcolor)
+    axT_ma= plt.axes([0.25, 0.125, 0.65, 0.01], facecolor=axcolor)
+    axT_fa= plt.axes([0.25, 0.15, 0.65, 0.01], facecolor=axcolor)
+
+    spim = Slider(axpim, 'p_immature', 0, 1, valinit=p_immature, valstep=.01)
+    spma = Slider(axpma, 'p_mature', .01, 1, valinit=p_mature, valstep=.01)
+    spfa = Slider(axpfa, 'p_facilitated', .01, 1, valinit=p_facilitated, valstep=.01)
+    sT_re = Slider(axT_re, 'T_refill', 1, 500, valinit=T_refill, valstep=20)
+    sT_ma = Slider(axT_ma, 'T_maturation', 1000, 4000, valinit=T_maturation, valstep=20)
+    sT_fa = Slider(axT_fa, 'T_facilitation', 1, 1000, valinit=T_facilitation, valstep=20)
 
     rax = plt.axes([0.025, 0.5, 0.15, 0.20], facecolor=axcolor)
     radio = RadioButtons(rax, ('1hz', '2hz', '10hz', '20hz', '50hz', '100hz'), active=0)
     plt.title("Now showing data for:")
 
     def update(val):
-        p_fast = spf.val
-        p_slow = sps.val
-        T_fast = sT_f.val
-        T_slow = sT_s.val
-        size_fast = sini_f.val
-        size_slow = 1-size_fast
-        delta_F = sdelta_F.val
+        p_immature = spim.val
+        p_mature = spma.val
+        p_facilitated = spfa.val
+        T_refill = sT_re.val
+        T_maturation = sT_ma.val
+        T_facilitation = sT_fa.val
 
         r = rs[radio.value_selected]
 
-        output = two_pool(r, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F)
-
-        EPSC_1 = two_pool(1, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F).EPSC
-        EPSC_2 = two_pool(2, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F).EPSC
-        EPSC_10 = two_pool(10, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F).EPSC
-        EPSC_20 = two_pool(20, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F).EPSC
-        EPSC_50 = two_pool(50, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F).EPSC
-        EPSC_100 = two_pool(100, n_pulses, size_fast, size_slow, p_fast, p_slow, T_fast, T_slow, delta_F, T_F).EPSC
+        EPSC_1 = maturation(1, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation).EPSC
+        EPSC_2 = maturation(2, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation).EPSC
+        EPSC_10 = maturation(10, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation).EPSC
+        EPSC_20 = maturation(20, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation).EPSC
+        EPSC_50 = maturation(50, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation).EPSC
+        EPSC_100 = maturation(100, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation).EPSC
 
         r_squared_1hz = metrics.r2_score(EPSC_1hz, EPSC_1)
         r_squared_2hz = metrics.r2_score(EPSC_2hz, EPSC_2)
@@ -112,23 +106,23 @@ if __name__ == "__main__":
         r_squared_50hz = metrics.r2_score(EPSC_50hz, EPSC_50)
         r_squared_100hz = metrics.r2_score(EPSC_100hz, EPSC_100)
 
-        avg_r_squared = (r_squared_1hz + r_squared_2hz+ r_squared_10hz + r_squared_20hz + r_squared_50hz + r_squared_100hz)/6
+        avg_r_squared = (r_squared_1hz + r_squared_2hz + r_squared_10hz + r_squared_20hz + r_squared_50hz + r_squared_100hz)/6
+        output = maturation(r, n_pulses, p_immature, p_mature, p_facilitated, T_refill, T_maturation, T_facilitation)
 
         print(avg_r_squared)
         l.set_ydata(output.EPSC)
-        m.set_ydata(output.fastEPSC)
-        n.set_ydata(output.slowEPSC)
+        m.set_ydata(output.EPSC_immature)
+        n.set_ydata(output.EPSC_mature)
+        o.set_ydata(output.EPSC_facilitated)
         q.set_ydata(EPSClabels[radio.value_selected])
         fig.canvas.draw_idle()
 
-    spf.on_changed(update)
-    sps.on_changed(update)
-    sT_f.on_changed(update)
-    sT_s.on_changed(update)
-    sini_f.on_changed(update)
-    sdelta_F.on_changed(update)
+    spim.on_changed(update)
+    spma.on_changed(update)
+    spfa.on_changed(update)
+    sT_re.on_changed(update)
+    sT_ma.on_changed(update)
+    sT_fa.on_changed(update)
     radio.on_clicked(update)
-
-    plt.xlim(0,n_pulses)
 
     plt.show()
